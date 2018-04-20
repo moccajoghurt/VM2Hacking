29575c4ae6fbb7b75427733805827b79466efd40
Marius <marius.anderie@hotmail.com>
2018-04-20 13:35:46 +0200

    added initial client

---
+++ Client/Client.cpp
@@ -0,0 +1,13 @@
+#include "Client.h"
+#include <iostream>
+
+using namespace std;
+
+void Client::Init() {
+    wstring we = L"";
+    lsassExe = we+L'l'+L's'+L'a'+L's'+L's'+L'.'+L'e'+L'x'+L'e';
+}
+
+int main() {
+
+}
---
+++ Client/Client.h
@@ -0,0 +1,11 @@
+#pragma once;
+#include <string>
+#include "../MemWars/MemWarsServices/StealthyMemManipulatorClient.h"
+#include "../MemWars/MemWarsServices/StealthyMemManipulatorGetHandleId.h"
+
+class Client {
+public:
+    void Init();
+    wstring lsassExe;
+    wstring vermintideExe;
+};
